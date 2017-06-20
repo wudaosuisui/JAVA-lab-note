8f2352d743b3de0db9468f353e2666fa728d5d4a
陈锐 <13081011450@163.com>
2017-06-20 17:35:11 +0800

    Create JiafenNianling.java
    加分数和中老年年龄的判断

---
+++ JiafenNianling.java
@@ -0,0 +1,59 @@
+# JAVA-lab-note
+lab &amp; note
+package HelloWord;
+
+public class Labe0507 {
+	
+public static void main(String[] args) {
+		int[] scoutr1 = {25,78,36,91};
+	
+		int[] scoutr2;
+		scoutr2 = new int[4];
+		int[] scoutr3 = new int[]{25,36,78,98};
+		
+		for(int i = 0;i<scoutr3.length;i++){
+			System.out.println(scoutr3[i]);
+		}
+		
+		/* // 变量保存成绩
+        int score = 53; 
+        
+        // 变量保存加分次数
+        int  count = 0 ;
+        //打印输出加分前成绩 
+          System.out.println("加分前成绩" + score);
+        // 只要成绩小于60，就循环执行加分操作，并统计加分次数
+          
+          int a[][] = new int[5][5];
+        if(score<60){
+        	//int count;
+            for(;score<60;count++ ){
+                score++;
+            }
+        }
+        //打印输出加分后成绩，以及加分次数
+        System.out.println("加分后成绩" + score);
+        System.out.println("加分次数"  + count);
+		*/
+		/*int score = 53;
+	
+		for(int  count = 0;score <60;count++){
+			score++;
+			
+		}
+		 System.out.println("加分后成绩" + score);
+	     System.out.println("加分次数"  + count);
+		*/
+		/*int age=16;
+		System.out.println(age >= 60 ? "老年" : age >= 40 && age < 60 ? 
+				"中年" : 18<= age || age<40 ? "少年" : "童年");
+	   if(age>60)System.out.println("老年");
+	    else if(age>40&&age<=60)System.out.println("中年");
+	    else if(age>18&&age<=40)System.out.println("少年");
+	    else if(age>0)System.out.println("童年");
+    */
+    
+    
+    
+	}
+}
