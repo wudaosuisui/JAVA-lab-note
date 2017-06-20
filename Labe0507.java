d078a763c345ac9b7b35f81109cdb4a380377197
陈锐 <13081011450@163.com>
2017-06-20 18:24:35 +0800

    Create Labe0507.java
    //_________for  foreach 的对比使用
    //_________Arrays 的简单使用

---
+++ Labe0507.java
@@ -0,0 +1,37 @@
+package HelloWord;
+
+import java.util.Arrays;
+
+public class Labe0507 {
+	
+public static void main(String[] args) {
+	
+	
+	
+//	//_________for  foreach 的对比使用
+//	//定义一个数组
+//	String[] hobbys = {"imooc","mokewang","www.imooc.com"};
+//	System.out.println("****使用for循环输出数组中元素:");
+//	for(int i =0;i<hobbys.length;i++){
+//		System.out.println(hobbys[i]);
+//	}
+//	System.out.println("****使用foreach循环输出数组中元素:");
+//	int i = 0;
+//	for (String hobby : hobbys){
+//		System.out.println("下标是："+i+"  内容是： "+hobby);
+//		i++;
+//	}//留个思考问题给大家：如果想在 foreach 语句中获取数组元素的下标，该如何做呢？？
+//	
+//	//_________Arrays 的简单使用
+//	// 定义一个字符串数组
+//	String[] hobbies = { "sports", "game", "movie" };
+//    
+//	// 使用Arrays类的sort()方法对数组进行排序
+//	Arrays.sort(hobbies);
+//    
+//	// 使用Arrays类的toString()方法将数组转换为字符串并输出
+//	System.out.println( Arrays.toString(hobbies));
+//    
+    
+	}
+}
